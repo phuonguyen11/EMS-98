e8a31508c16474d8a270d307d6f65ab911e6d102
Toan Vo <vothaitoan12@gmail.com>
2024-04-08 11:40:34 +0700

    feat: add update profile hook, load course,auth

--- app/src/hooks/authentication.js
+++ app/src/hooks/authentication.js
@@ -1,5 +1,5 @@
-import { auth } from '../../firebase';
-export const logOut = () => {
+import { signInWithEmailAndPassword, signOut, updateEmail, updatePassword } from 'firebase/auth';
+export const logOut = (auth) => {
   signOut(auth)
     .then(() => {
       navigate('/login');
@@ -8,12 +8,32 @@ export const logOut = () => {
       console.log(error);
     });
 };
-export const signIn = (e) => {
+export const signIn = (e, auth) => {
   e.preventDefault();
   // get email and password from form input
   signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       console.log('user data: ', userCredential);
+      navigate('/');
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+export const updateAuthEmail = (auth, newEmail) => {
+  updateEmail(auth.currentUser, newEmail)
+    .then(() => {
+      console.log('email updated successfully');
+      navigate('/');
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+export const updateAuthPassword = (auth, newPassword) => {
+  updatePassword(auth.currentUser, newPassword)
+    .then(() => {
+      console.log('password updated successfully');
       navigate('/');
     })
     .catch((error) => {
