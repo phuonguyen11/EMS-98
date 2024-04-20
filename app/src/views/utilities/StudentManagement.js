0b910fe71354c425023e1cb8761a6d02118ee7d2
uyen.banh <phuonguyen11112002@gmail.com>
2024-04-20 15:57:34 +0700

    done admin page

--- app/src/views/utilities/StudentManagement.js
+++ app/src/views/utilities/StudentManagement.js
@@ -1,18 +1,28 @@
 // project imports
+import { useState, useEffect } from 'react';
 import MainCard from 'ui-component/cards/MainCard';
 import SecondaryAction from 'ui-component/cards/CardSecondaryAction';
 
 // assets
 import LinkIcon from '@mui/icons-material/Link';
-import Table from 'ui-component/Table';
-
-// =============================|| TABLER ICONS ||============================= //
+import { loadUserByRole } from 'hooks/loadUserByRole';
+import { CircularProgress } from '@mui/material';
+import StudentTable from 'ui-component/StudentTable';
 
 const StudentManagement = () => {
-  
+  const [students, setStudents] = useState([]);
+
+  useEffect(async() => {
+    const studentList = await loadUserByRole("student");
+    if (studentList) {
+      setStudents(studentList);
+    }
+  }, []);
+
+
   return (
   <MainCard title="Student Management" secondary={<SecondaryAction icon={<LinkIcon fontSize="small" />} link="https://tablericons.com/" />}>
-    <Table/>
+    {students? <StudentTable data={students} />: <CircularProgress/>}
   </MainCard>
 )};
 
--- app/src/views/utilities/TeacherManagement.js
+++ app/src/views/utilities/TeacherManagement.js
@@ -1,19 +1,28 @@
+import { useState, useEffect } from 'react';
 // project imports
 import MainCard from 'ui-component/cards/MainCard';
 import SecondaryAction from 'ui-component/cards/CardSecondaryAction';
 
 // assets
 import LinkIcon from '@mui/icons-material/Link';
-import Table from 'ui-component/Table';
+import TeacherTable from 'ui-component/TeacherTable';
+import { loadUserByRole } from 'hooks/loadUserByRole';
+import { CircularProgress } from '@mui/material';
 
+const TeacherManagement = () => {
+  const [teachers, setTeachers] = useState([]);
 
-// =============================|| TABLER ICONS ||============================= //
+  useEffect(async() => {
+    const teacherList = await loadUserByRole("teacher");
+    if (teacherList) {
+      setTeachers(teacherList);
+    }
+  }, []);
 
-
-const TeacherManagement = () => (
-  <MainCard title="Teacher Management" secondary={<SecondaryAction icon={<LinkIcon fontSize="small" />} link="https://tablericons.com/" />}>
-    <Table/>
-  </MainCard>
-);
+  return (
+    <MainCard title="Teacher Management" secondary={<SecondaryAction icon={<LinkIcon fontSize="small" />} link="https://tablericons.com/" />}>
+      {teachers? <TeacherTable data={teachers} />: <CircularProgress/>}
+    </MainCard>
+  )};
 
 export default TeacherManagement;
